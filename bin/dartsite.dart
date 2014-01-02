b4cdd3dfb5016c031b2b31fbb566fcc279faf6c8
James Wendel <jmwendel@gmail.com>
2014-01-01 22:14:24 -0600

    Added args package.  playing with args!

--- bin/dartsite.dart
+++ bin/dartsite.dart
@@ -1,3 +1,10 @@
-void main() {
-  print("Hello, World!");
+import 'package:args/args.dart';
+
+void main(List<String> args) {
+  ArgParser parser = new ArgParser();
+  parser.addFlag("add", abbr: "a");
+  ArgResults result = parser.parse(args);
+
+  List<String> files = result.rest;
+  print(files);
 }
